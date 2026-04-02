1893e7f30cf0474755165cfebe2fc89382c45b60
z3roco01 <zerocool@e.email>
2026-04-02 00:31:39 -0500

    now preserves options from newer versions, in older versions

--- src/main/java/z3roco01/syncywinky/mixin/client/OptionsMixin.java
+++ src/main/java/z3roco01/syncywinky/mixin/client/OptionsMixin.java
@@ -1,7 +1,13 @@
 package z3roco01.syncywinky.mixin.client;
 
+import net.minecraft.SharedConstants;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Options;
+//? if >=1.19.2 {
+import net.minecraft.client.OptionInstance;
+//?}
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,6 +30,24 @@ public class OptionsMixin {
 
     @Unique
     private static File globalOptionsFile;
+    // backport old settings so they wont get overwritten
+    // all added after 1.18
+    //? if <=1.18.2 {
+    /*@Unique
+    private static double darknessEffectScale;
+    @Unique
+    private static boolean directionalAudio;
+    *///?}
+    //? if <=1.19.2 {
+    /*@Unique
+    private static boolean narratorHotkey;
+    *///?}
+    //? if <=1.21.1 {
+    @Unique
+    private static String musicFrequency;
+    @Unique
+    private static String musicToast;
+    //?}
 
     @Inject(method = "<init>", at = @At("HEAD"))
     private static void init(Minecraft minecraft, File workingDirectory, CallbackInfo ci) {
@@ -36,6 +60,18 @@ public class OptionsMixin {
             }
             SyncyWinkyClient.LOGGER.info("Created directory for config with path: " + globalOptionsFile.getParentFile().toString());
         }
+
+        //? if <=1.18.2 {
+        /*directionalAudio = false;
+        darknessEffectScale = 1.0;*/
+        //?}
+        //? if <=1.19.2 {
+        /*narratorHotkey = true;
+        *///?}
+        //? if <=1.21.1 {
+        musicFrequency = "DEFAULT";
+        musicToast = "never";
+        //?}
     }
 
     @Redirect(method = "load", at = @At(value = "FIELD", target = "Lnet/minecraft/client/Options;optionsFile:Ljava/io/File;", opcode = Opcodes.GETFIELD))
@@ -53,5 +89,26 @@ public class OptionsMixin {
     private void getFile(CallbackInfoReturnable<File> cir) {
         cir.setReturnValue(globalOptionsFile);
         cir.cancel();
+    }
+
+    // Dont datafix, preserves non existant options from newer versions
+    @Inject(method = "dataFix", at = @At("HEAD"), cancellable = true)
+    private void dataFix(CompoundTag compoundTag, CallbackInfoReturnable<CompoundTag> cir) {
+    }
+
+    // make backported options loaded/stored
+    @Inject(method = "processOptions", at = @At("TAIL"))
+    private void processOptions(Options.FieldAccess fieldAccess, CallbackInfo ci) {
+        //? if <=1.18.2 {
+        /*directionalAudio = fieldAccess.process("directionalAudio", directionalAudio);
+        darknessEffectScale = fieldAccess.process("darknessEffectScale", darknessEffectScale);
+        *///?}
+        //? if <=1.19.2 {
+        /*narratorHotkey = fieldAccess.process("narratorHotkey", narratorHotkey);
+        *///?}
+        //? if <=1.21.1 {
+        musicFrequency = fieldAccess.process("musicFrequency", musicFrequency);
+        musicToast = fieldAccess.process("musicToast", musicToast);
+        //?}
     }
 }
